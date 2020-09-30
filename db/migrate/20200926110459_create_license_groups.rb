05c31efe1694745f458f72e92c4e9e7066722262
古瀬　就悟 <shugoena@gmail.com>
2020-09-30 15:08:44 +0900

    WIP

--- db/migrate/20200926110459_create_license_groups.rb
+++ db/migrate/20200926110459_create_license_groups.rb
@@ -1,7 +1,8 @@
 class CreateLicenseGroups < ActiveRecord::Migration[6.0]
   def change
     create_table :license_groups do |t|
-      t.integer :card_id, null: false
+      t.integer :card_id
+      t.integer :music_card_id
       t.integer :number_of_licenses, null: false
       t.timestamps
     end
